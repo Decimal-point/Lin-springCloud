45c61a57b00ac701a826fb018d3b942737b4bdc0
lin <tcslforone@163.com>
2018-02-01 18:55:28 +0800

    Lin

--- Lin-springCloud-producer/src/main/java/com/lin/producer/Application.java
+++ Lin-springCloud-producer/src/main/java/com/lin/producer/Application.java
@@ -1,11 +1,13 @@
 package com.lin.producer;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-/*@ComponentScan("com.linx")
-@EnableAutoConfiguration*/
+@ComponentScan("com.linx")
+@EnableAutoConfiguration
 /*@Configuration
 @Controller*/
 public class Application {
